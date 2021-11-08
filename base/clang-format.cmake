b3bcdac80ff1614fc59e1470539ae546842f94f0
Sung Cho <sung.w.cho@pm.me>
2021-11-07 19:41:15 -0800

    fixed passing multi paths

--- base/clang-format.cmake
+++ base/clang-format.cmake
@@ -1,13 +1,27 @@
-macro(post_build_clang_format target src_files)
+macro(post_build_clang_format target)
     find_file(CLANG_FORMAT_PATH
         clang-format
+    )
+
+    # make string list of all parameters after the target name
+    # expected to be source file paths relative to current dir
+    # add to list as absolute paths
+    set(file_path_list)
+    foreach(file_name ${ARGN})
+        list(APPEND file_path_list "${CMAKE_CURRENT_LIST_DIR}/${file_name}")
+    endforeach()
+    # join list with ; delimiter
+    list(JOIN file_path_list
+        ";"
+        src_files
     )
 
     if (EXISTS "${CLANG_FORMAT_PATH}")
         add_custom_command(
             TARGET ${target}
             POST_BUILD
-            COMMAND python ${CMAKE_SOURCE_DIR}/scripts/clang-format.py --src-files "${src_files}"
+            # pass paths as a single quoted string
+            COMMAND python ${CMAKE_SOURCE_DIR}/scripts/clang-format.py --src-files "'${src_files}'"
         )
     else()
         message("clang-format not found")
