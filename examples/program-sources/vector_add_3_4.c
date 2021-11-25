0c61d0f8872d37fa69457d081f81195a9abdce1c
Sung Cho <sung.w.cho@pm.me>
2021-11-24 19:23:25 -0800

    c style comments

--- examples/program-sources/vector_add_3_4.c
+++ examples/program-sources/vector_add_3_4.c
@@ -1,8 +1,8 @@
 __kernel
 void vector_add_3_4(__global int* A, __global int* B, __global int* C)
 {
-    // partitioned such that work item id = vector index
+    /* partitioned such that work item id = vector index */
     int idx = get_global_id(0);
-    // store computation result in output vector C
+    /* store computation result in output vector C */
     C[idx] = A[idx] + B[idx];
 }
